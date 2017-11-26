8dd45ea04ae82334299046e9bda778dd90d46612
Adam Kaczmarek <bja1011@gmail.com>
2017-11-26 22:26:57 +0100

    Material-UI integrated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Routes from "./Routes";
 import Header from "./components/Header/Header";
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ import React, {Component} from 'react';
 import {INDEX_PATH, MANAGE_PATH, SETTINGS_PATH} from "../../constants/RouterConstants";
 import {Link} from "react-router-dom";
 import './Header.css';
+import {Button} from "material-ui";
 
 /**
  * Main navigation component
@@ -12,11 +13,9 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="App-header">
-      <ul>
-        <li><Link to={{pathname: INDEX_PATH}}>Dashboard</Link></li>
-        <li><Link to={{pathname: MANAGE_PATH}}>Manage</Link></li>
-        <li><Link to={{pathname: SETTINGS_PATH}}>Settings</Link></li>
-      </ul>
+      <Link to={{pathname: INDEX_PATH}}><Button raised color="primary">Dashboard</Button></Link>
+      <Link to={{pathname: MANAGE_PATH}}><Button raised color="primary">Manage</Button></Link>
+      <Link to={{pathname: SETTINGS_PATH}}><Button raised color="primary">Settings</Button></Link>
     </header>
   )
 }
